70818fb06d405405232639fad8595eeba7cd6416
sampazzer <sampazzer@googlemail.com>
2018-08-25 03:32:07 +0100

    Changes to gameplay. Now has an endgame and timer shown.

--- explosion.js
+++ explosion.js
@@ -5,15 +5,24 @@ class explosion{
         this.ypos = round(random(25,475));
         this.spawned = false;
         this.exploded = false;
+		this.explodedsize = 250;
         this.is_destroyed = false;
         this.distance = 0;
         this.growrate = 0;
+		this.freeze = false;
     }
     
     show(){
-        fill('red');
-        noStroke();
-        ellipse(this.xpos,this.ypos,this.size);
+		if (this.size <= 200){
+			fill('green');
+			noStroke();
+			ellipse(this.xpos,this.ypos,this.size);
+		}
+		else{
+			fill('red');
+			noStroke();
+			ellipse(this.xpos,this.ypos,this.size);
+		}
     }
     
     spawn(spawn_called){
@@ -24,7 +33,7 @@ class explosion{
     }
     
     collided_with(mouseyx,mouseyy){
-        if (this.spawned == true && this.is_destroyed == false){
+        if (this.spawned == true && this.is_destroyed == false && !this.exploded){
             let collision_distance = this.size/2
             this.distance = dist(mouseyx,mouseyy,this.xpos,this.ypos);
             if(this.distance < collision_distance){
@@ -34,13 +43,24 @@ class explosion{
         }
     }
     
-    
     update(howfast){
         //only updates if its spawned and not destroyed. Once destroyed it does not update.
         if (this.spawned == true && this.is_destroyed == false){
-            this.size = this.size + howfast;
-            fill('red');
-            ellipse(this.xpos,this.ypos,this.size);
+			if (!this.exploded){
+				this.size = this.size + howfast;
+				this.explodegameend();
+			}
+			this.show();
         }
     }
+	
+	
+	
+	explodegameend(){
+		if (this.size > this.explodedsize){
+			this.exploded = true;
+			console.log("game ended");
+		}
+	}
+	
 }
--- sketch.js
+++ sketch.js
@@ -1,33 +1,53 @@
 let explosionarray = [];
+let noofexplosions = 9;
 let spawntimer = 3000;
+let endthegame = false;
+let txt;
+let timertext;
+
 
 function setup(){
     createCanvas(500,500);
     //background(150);
-    for(var i = 0; i < 9; i++){
+    for(var i = 0; i < noofexplosions; i++){
         explosionarray[i] = new explosion(50);
     }
+	txt = createDiv('Hello');
+	txt.position(600, 50);
+	timertxt = createDiv();
+	timertxt.position(600, 100);
 }
     
 function draw(){
     background(150);
-    let gametimer = millis();
-    if (gametimer >= spawntimer){
-        console.log("spawn one");
-        for(var i = 0; i < 9; i++){
-            //check for spawned status. If not spawned and the gametimer is above spawn timer then spawn an explosion.
-            if (explosionarray[i].spawned == false){
-                explosionarray[i].spawn(true);
-                break;
-            }
-        }
-        spawntimer = spawntimer + random([2000,3000,4000,5000]);
-    }
-    
-    for (var i = 0; i < 9; i++){
+	if (endthegame){
+		txt.html("Game Ended");
+	}
+	if (!endthegame){
+		let gametimer = millis();
+		if (gametimer >= spawntimer){
+			console.log("spawn one");
+			for(var i = 0; i < noofexplosions; i++){
+				//check for spawned status. If not spawned and the gametimer is above spawn timer then spawn an explosion.
+				if (explosionarray[i].spawned == false){
+					explosionarray[i].spawn(true);
+					break;
+				}
+			}
+			spawntimer = spawntimer + random([2000,3000,4000,5000]);
+		}
+		timertxt.html("You survived: " +gametimer/1000);
+	}
+	
+    for (var i = 0; i < noofexplosions; i++){
         //check for collision with mouse. only checks if spawned is true and destroyed is false (in explosion.js).
-        explosionarray[i].update(0.5);
-        explosionarray[i].collided_with(mouseX,mouseY);
-        
+		if (endthegame && explosionarray[i].exploded == false){
+			explosionarray[i].exploded = true;
+		}
+		explosionarray[i].update(5);
+		explosionarray[i].collided_with(mouseX,mouseY);
+		if (explosionarray[i].exploded == true){
+		endthegame = true;	
+		}
     }
 }
